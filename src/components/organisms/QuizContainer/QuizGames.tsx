58ca8f810b66bcf393a575c3e11954566b248cbc
yttiiz <dominique.talis@proton.me>
2024-09-09 02:42:46 +0200

    feat(component): create warning component

--- src/components/organisms/QuizContainer/QuizGames.tsx
+++ src/components/organisms/QuizContainer/QuizGames.tsx
@@ -3,7 +3,7 @@ import { Fetcher } from "@/utils/mod";
 
 export const QuizGames = async () => {
 	const { __NEXT_PRIVATE_ORIGIN: host } = process.env;
-	const response = await Fetcher.postData(`${host}/api/jsonr`, {
+	const response = await Fetcher.postData(`${host}/api/json`, {
 		file: "select-quiz",
 	});
 
