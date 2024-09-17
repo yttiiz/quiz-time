f7ba7592f8d7291ff171d71a05e083ce13ddb99c
yttiiz <dominique.talis@proton.me>
2024-09-17 17:28:59 +0200

    feat(component): improve 'QuizResult' component

--- src/components/molecules/QuizResult/QuizResult.tsx
+++ src/components/molecules/QuizResult/QuizResult.tsx
@@ -1,8 +1,22 @@
 export const QuizResult = ({ points, length }: { points: number; length: number }) => {
-  return (
-    <div>
-      <h3>Résultat final</h3>
-      <div>Vous avez fait un score de {points} sur {length}.</div>
-    </div>
-  );
+  points = +((points / length) * 20).toFixed();
+
+	const congrats = (note: number) => {
+		if (note >= 16) return "Félicitations";
+		if (note >= 12 && note < 16) return "C'est très bien";
+		if (note >= 10 && note < 10) return "C'est assez bien";
+		if (note >= 8 && note < 10) return "C'est moyen";
+		return "C'est à revoir";
+	};
+
+	return (
+		<div className="grid gap-4">
+			<h3>Résultat final</h3>
+			<div>
+				Vous avez obtenu la note de{" "}
+				<b className="text-alert-default">{points} sur 20</b>.{" "}
+				{congrats(points)} !
+			</div>
+		</div>
+	);
 };
