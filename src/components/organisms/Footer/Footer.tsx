b96000218dc4f194dadcff5d00bd080f4107329a
yttiiz <dominique.talis@proton.me>
2024-09-19 14:19:04 +0200

    refactor(footer): WIP - improve footer appearance

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -17,31 +17,37 @@ export const Footer = async ({
 	);
 
 	let copyrights: string = "copyrights not found";
-	let links: ItemsType[] = [];
+  let author = {} as ItemsType;
+	let links = [];
 
 	if (footeResponse.ok) {
 		copyrights = footeResponse.data["copyrights"];
+		author = { ...(footeResponse.data["author"] as unknown as ItemsType) };
 	}
 
 	if (response.ok) {
 		links = [...response.data["items"]] as unknown as ItemsType[];
 	} else {
-		links = [{ textContent: "home", url: "/" }];
+		links = [{ textContent: "accueil", url: "/" }];
 	}
 
 	return (
 		<footer className="bg-primary-content/10">
 			<div className="container grid gap-6 py-6">
-				<ul>
-					{links.map(({ textContent, url }) => (
-						<li key={(Math.random() + 1) * 1000}>
-							<a href={url}>{textContent}</a>
-						</li>
-					))}
-				</ul>
+        <div>
+          <h3 className="mb-2">Catégories</h3>
+          <ul>
+            {links.map(({ textContent, url }) => (
+              <li key={(Math.random() + 1) * 1000}>
+                <a href={url} className="hover:text-alert-default">{textContent}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
 				<div className="text-center border-t-2 pt-4 border-primary-content/10">
 					<span className="text-primary-content/40">
-						{copyrights} {new Date().getUTCFullYear()}
+						{copyrights}{" "}
+						{copyrights.includes("not found") ? "" : new Date().getUTCFullYear()}
 					</span>
 				</div>
 			</div>
