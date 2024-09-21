5f9ff8b4f05ace107c8eea7e77e1c4c4e80e1fb0
yttiiz <dominique.talis@proton.me>
2024-09-21 13:48:44 +0200

    feat(mongodb): create method to handle user connexion

--- src/app/api/mongodb/user/route.ts
+++ src/app/api/mongodb/user/route.ts
@@ -1,14 +1,23 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { RouteHelper } from "@/utils/route-helper";
-import { Mongo } from "@/services/mod";
+import { Mongo, UserSchemaType } from "@/services/mod";
 
 export async function POST(req: NextRequest) {
   const { value, isValueAstring } = await RouteHelper.getValueAsString(req);
 
 	if (isValueAstring) {
 		const { email, password } = JSON.parse(value);
-    // Do stuff
+		const user = await Mongo.getDocumentFrom<UserSchemaType>({
+			db: "main",
+			collection: "users",
+			key: "email",
+			identifier: email,
+		});
+
+		if (user && await Mongo.isPasswordOk(password, user.hash)) {
+			
+		}
 	}
 
 	return NextResponse.json({ message: "Incorrect value given." });
--- src/services/mongo/types.ts
+++ src/services/mongo/types.ts
@@ -1,10 +1,16 @@
 import { ObjectId } from "mongodb";
 
-export type GetDocumentsParameterType = {
+export type GetDocumentsFromParameterType = {
 	db: string;
 	collection: string;
 };
 
+export type GetDocumentFromParameterType<T> = GetDocumentsFromParameterType & {
+	identifier: string;
+	key: keyof T;
+};
+
+// Question Schema
 export type QuestionDetailsType = {
 	title: string;
 	propositions: string[];
@@ -14,4 +20,19 @@ export type QuestionType = {
 	_id: ObjectId;
 	question: QuestionDetailsType;
 	correction: string;
+};
+
+// User Schema
+export type UserType = {
+	firstname: string;
+	lastname: string;
+	email: string;
+	role: string;
+	job?: string;
+	photo?: string;
+	birth?: Date;
+};
+
+export type UserSchemaType = UserType & {
+	hash: string;
 };
