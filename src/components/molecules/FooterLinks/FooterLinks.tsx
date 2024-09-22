e78c2805c0cac65d32c3eb121eb28f2b41fc84fb
yttiiz <dominique.talis@proton.me>
2024-09-22 11:52:08 +0200

    chore(footer): remove 'connexion' in footer if user is connected

--- src/components/molecules/FooterLinks/FooterLinks.tsx
+++ src/components/molecules/FooterLinks/FooterLinks.tsx
@@ -1,6 +1,16 @@
+"use client";
+
 import { ItemType } from "@/components/mod";
+import { useUserDataStore } from "@/store/mod";
 
 export const FooterLinks = ({ links }: { links: ItemType[] }) => {
+	const firstname = useUserDataStore((state) => state.firstname);
+
+	// If user is connected, remove 'connexion' item in the list.
+	if (firstname) {
+		links = links.filter(links => links.textContent !== "Connexion")
+	}
+
 	return (
 		<div>
 			<h3 className="mb-2">Catégories</h3>
