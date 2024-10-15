a516769b9c10834f40f4d98f703f30f4ae2a6890
yttiiz <dominique.talis@proton.me>
2024-10-15 08:37:13 -0400

    chore(data): change relative path to data directory

--- src/app/api/json/route.ts
+++ src/app/api/json/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
 	const stringifyData = (await req.formData()).get("value");
 	const { file } = JSON.parse(stringifyData as string);
-	const data = await FileHelper.convertJsonToObject(`src/data/${file}.json`);
+	const data = await FileHelper.convertJsonToObject(`./src/data/${file}.json`);
 
 	return NextResponse.json(data);
 }
