bea792fbc10503cedd14861d488cea3b8591f340
yttiiz <dominique.talis@proton.me>
2024-10-22 17:51:11 -0400

    feat(session): wrap app with 'SessionProvider' to get user data anywhere in client component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SessionProvider } from "next-auth/react";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 
@@ -20,8 +21,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <SessionProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </SessionProvider>
   );
 }
