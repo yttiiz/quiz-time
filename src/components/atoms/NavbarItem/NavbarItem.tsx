3fce113053f09b8f2aa616401d9cf50c82ae68c0
yttiiz <dominique.talis@proton.me>
2024-10-26 16:00:21 -0400

    feat(user-connexion): improve navbar display after deconnexion

--- src/components/atoms/NavbarItem/NavbarItem.tsx
+++ src/components/atoms/NavbarItem/NavbarItem.tsx
@@ -1,34 +1,13 @@
 "use client";
 
 import { IconOnOff } from "../mod";
-import { ItemType, NavbarItemPropsType } from "../types";
-import { store, useHeaderStore } from "@/store/mod";
-import { SetterType } from "@/store/types";
-import { Fetcher } from "@yttiiz/utils";
+import { NavbarItemPropsType } from "../types";
 import { handleSignOut } from "@/actions/authActions";
 
 export const NavbarItem = (
 	{ textContent, url, isFormConnexion }: NavbarItemPropsType,
 	key: string,
 ) => {
-	const [setItems] = store(useHeaderStore, "setItems") as [
-		SetterType<ItemType[]>,
-	];
-
-	const onSubmit = () => {
-		Fetcher.postData<{ items: ItemType[] }>(
-			`${globalThis.location.origin}/api/json`,
-			{
-				file: "header",
-			},
-			"next",
-		)
-			.then((res) => (res.ok ? res.data : res.message))
-			.then((data) =>
-				typeof data !== "string" ? setItems(data["items"]) : null,
-			);
-	};
-
 	return (
 		<li
 			key={key}
@@ -37,7 +16,6 @@ export const NavbarItem = (
 			{isFormConnexion ? (
 				<form
 					action={handleSignOut}
-					onSubmit={onSubmit}
 				>
 					<button
 						type="submit"
